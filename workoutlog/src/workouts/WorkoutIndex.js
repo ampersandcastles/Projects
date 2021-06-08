85cef21ddf769c33e6222d5313568af1ac8e0432
Tristan Smith <tristan.smith@gmail.com>
2021-06-07 21:42:32 -0400

    major confusion

---
+++ workoutlog/src/workouts/WorkoutIndex.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+const WorkoutIndex = (props) => {
+  const [workouts, setWorkouts] = useState([]);
+
+  return <div>Workout Index</div>;
+};
+
+export default WorkoutIndex;
